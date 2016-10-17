55b94e0888f28f8a889593a22a157747e2cb8240
ben slama <ben.slama.mohamed.ali@gmail.com>
2016-10-17 11:43:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+elastic
+=======
+
+A Symfony project created on October 17, 2016, 10:01 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Elastica\Query\QueryString;
+use Elastica\Filter\Query;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        
+        $finder = $this->container->get('fos_elastica.finder.user');
+        
+        $results = $finder->find('xxx');
+        
+        
+        // find articles with "like" query
+        $keywordQuery = new QueryString();
+        $keywordQuery->setQuery("*t*");
+
+        $query = new Query();
+        $query->setQuery($keywordQuery);
+
+        $articles = $finder->find($query);
+        
+        dump($articles);
+        
+        
+        exit;
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+}
